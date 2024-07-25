54499ccb9fe8653e55e4a90e827a4c0ee37f8f5a
Kapil <kapilpalanivel1@gmail.com>
2024-07-25 09:13:26 +0530

    Client Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './Componenets/Common/Navbar/navbar';
+import ClientLogin from './Componenets/Client/ClientLogin';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <ClientLogin/>
     </div>
   );
 }
