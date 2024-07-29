192903b4d35ee12cbb5cdd8ce26e48b79c823940
Kapil <kapilpalanivel1@gmail.com>
2024-07-29 09:25:13 +0530

    Footer-complet

--- src/Componenets/Footer/Footer.jsx
+++ src/Componenets/Footer/Footer.jsx
@@ -69,10 +69,10 @@ const Footer = () => {
             </div>
             <div className="footer-section">
               <h4 className="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4>
-              <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+              <p>Subscribe To our News Letter.</p>
               <div className="position-relative mx-auto" style={{ maxWidth: 400 }}>
-                <input className="form-control border-primary w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email" />
-                <button type="button" className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                <input style={{borderRadius:"5px"}} className="form-control border-primary w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email" />
+                <button type="button" style={{borderRadius:"5px"}} className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
               </div>
             </div>
           </div>
